c0a85b3924b6121b9c9186a3e7994c7b1525cabb
Ahllaine Christian De Ocera <acdeocera.dev@gmail.com>
2026-01-09 09:24:42 +0800

    test(auth): add feature tests for authentication flows

---
+++ tests/Feature/Auth/LogoutTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Models\User;
+
+test('users can logout', function () {
+    $user = User::factory()->create();
+
+    $response = $this->actingAs($user)->post(route('logout'));
+
+    $this->assertGuest();
+    $response->assertRedirect(route('home'));
+});
