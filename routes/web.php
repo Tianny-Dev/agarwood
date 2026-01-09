ccc17cce221d5a7b2b0ea7f152f29ee25736a4d1
Ahllaine Christian De Ocera <acdeocera.dev@gmail.com>
2026-01-09 14:05:49 +0800

    fix(routes): renamed home page route to home only

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Dashboard\DashboardController;
 use App\Http\Controllers\Home\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])
