97c4b19c520fd431090777a0e7c82b9f6977edea
Ahllaine Christian De Ocera <acdeocera.dev@gmail.com>
2026-01-09 09:57:55 +0800

    feat(ui):Implement Inertia app and guest layouts with home and dashboard pages

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import GuestLayout from '@/layouts/GuestLayout.vue';
+import { login, register } from '@/routes';
+import { index as dashboard } from '@/routes/dashboard'
+import { Head, Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Home" />
+    <GuestLayout>
+        <div class="min-h-full flex items-center justify-center ">
+            <div class="max-w-md w-full rounded-lg p-8 text-center">
+                <h1 class="text-3xl font-bold mb-4 text-gray-800">Welcome</h1>
+                <p class="text-gray-600 mb-6">Hello, welcome to Agarwood!</p>
+                <div class="flex gap-2">
+                    <Link
+                        v-if="$page.props.auth.user"
+                        :href="dashboard()"
+                        class="inline-block w-full bg-amber-950 text-white font-medium py-2 px-4 rounded hover:bg-amber-900 transition"
+                    >
+                        Dashboard
+                    </Link>
+
+                    <template v-else>
+                        <Link
+                            :href="login()"
+                            class="inline-block w-full bg-amber-950 text-white font-medium py-2 px-4 rounded hover:bg-amber-900 transition"
+                        >
+                            Login
+                        </Link>
+                        <Link
+                            :href="register()"
+                            class="inline-block w-full bg-amber-950 text-white font-medium py-2 px-4 rounded hover:bg-amber-900 transition"
+                        >
+                            Register
+                        </Link>
+                    </template>
+                </div>
+            </div>
+        </div>
+    </GuestLayout>
+</template>
---
+++ resources/js/pages/dashboard/Index.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { Head, router } from '@inertiajs/vue3';
+
+const handleLogout = () => {
+    router.flushAll();
+};
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AppLayout>
+        <template #header>
+            <h2
+                class="text-xl font-semibold leading-tight text-gray-800"
+            >
+                Dashboard
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div
+                    class="overflow-hidden bg-white sm:rounded-lg"
+                >
+                    <div class="p-6 text-gray-900">
+                        Protected Routes here
+                    </div>
+                    <button
+                        @click="handleLogout()"
+                        class="inline-block w-full bg-amber-950 text-white font-medium py-2 px-4 rounded hover:bg-amber-900 transition cursor-pointer"
+                    >
+                        Logout
+                    </button>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
