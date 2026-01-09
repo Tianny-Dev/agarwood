0eaefa0932f6bce72060d92b628def604ac613df
Ahllaine Christian De Ocera <acdeocera.dev@gmail.com>
2026-01-09 17:51:35 +0800

    fix: Use the reusable TextLink component

--- resources/js/pages/auth/Login.vue
+++ resources/js/pages/auth/Login.vue
@@ -1,25 +1,37 @@
 <script setup lang="ts">
-import { useForm, Link, Head } from '@inertiajs/vue3'
+import { store } from '@/actions/App/Http/Controllers/Auth/LoginController';
 import AuthLayout from '@/layouts/AuthLayout.vue';
-import { store } from '@/actions/App/Http/Controllers/Auth/LoginController'
+import { register } from '@/routes';
+import { request } from '@/routes/password';
+import { Head, useForm } from '@inertiajs/vue3';
 
-const showPassword = ref(false)
+const showPassword = ref(false);
 
 const form = useForm({
     email: null,
     password: null,
-})
+});
 
 function submit() {
-    form.submit(store())
+    form.submit(store());
 }
+
+defineProps<{
+    status?: string;
+    canResetPassword: boolean;
+}>();
 </script>
 
 <template>
     <Head title="Register" />
 
     <AuthLayout>
-        <h2 class="text-2xl font-semibold text-center mb-4">Login to your account</h2>
+        <h2 class="mb-4 text-center text-2xl font-semibold">Login to your account</h2>
+
+        <div v-if="status" class="mb-4 text-center text-sm font-medium text-green-600">
+            {{ status }}
+        </div>
+
         <form @submit.prevent="submit" class="flex flex-col gap-6">
             <div class="grid gap-6">
                 <UFormField name="email" :error="form.errors.email" label="Email address">
@@ -27,27 +39,30 @@ function submit() {
                 </UFormField>
 
                 <UFormField name="password" :error="form.errors.password" label="Password">
-                  <UInput
-                    v-model="form.password"
-                    class="w-full"
-                    :type="showPassword ? 'text' : 'password'"
-                    autocomplete="new-password"
-                    placeholder="Password"
-                    :ui="{ trailing: 'pe-1' }"
-                    required
-                  >
-                    <template #trailing>
-                      <UButton
-                        color="neutral"
-                        variant="link"
-                        size="sm"
-                        :icon="showPassword ? 'i-lucide-eye-off' : 'i-lucide-eye'"
-                        :aria-label="showPassword ? 'Hide password' : 'Show password'"
-                        :aria-pressed="showPassword"
-                        @click="showPassword = !showPassword"
-                      />
+                    <UInput
+                        v-model="form.password"
+                        class="w-full"
+                        :type="showPassword ? 'text' : 'password'"
+                        autocomplete="new-password"
+                        placeholder="Password"
+                        :ui="{ trailing: 'pe-1' }"
+                        required
+                    >
+                        <template #trailing>
+                            <UButton
+                                color="neutral"
+                                variant="link"
+                                size="sm"
+                                :icon="showPassword ? 'i-lucide-eye-off' : 'i-lucide-eye'"
+                                :aria-label="showPassword ? 'Hide password' : 'Show password'"
+                                :aria-pressed="showPassword"
+                                @click="showPassword = !showPassword"
+                            />
+                        </template>
+                    </UInput>
+                    <template #hint>
+                        <TextLink v-if="canResetPassword" :href="request()" class="text-sm text-primary" :tabindex="5"> Forgot password? </TextLink>
                     </template>
-                  </UInput>
                 </UFormField>
 
                 <UButton :loading="form.processing" type="submit" block class="mt-4">Log in</UButton>
@@ -56,7 +71,7 @@ function submit() {
 
         <p class="mt-4 text-center text-sm text-gray-600">
             Don't have an account?
-            <Link href="/register" class="text-blue-500 hover:underline">Register</Link>
+            <TextLink :href="register()" class="text-sm font-medium text-primary">Register</TextLink>
         </p>
     </AuthLayout>
 </template>
--- resources/js/pages/auth/Register.vue
+++ resources/js/pages/auth/Register.vue
@@ -1,20 +1,21 @@
 <script setup lang="ts">
-import { useForm, Link, Head } from '@inertiajs/vue3'
+import { store } from '@/actions/App/Http/Controllers/Auth/RegisterController';
 import AuthLayout from '@/layouts/AuthLayout.vue';
-import { store } from '@/actions/App/Http/Controllers/Auth/RegisterController'
+import { login } from '@/routes';
+import { Head, useForm } from '@inertiajs/vue3';
 
-const showPassword = ref(false)
-const showPasswordConfirmation = ref(false)
+const showPassword = ref(false);
+const showPasswordConfirmation = ref(false);
 
 const form = useForm({
     name: null,
     email: null,
     password: null,
     password_confirmation: null,
-})
+});
 
 function submit() {
-    form.submit(store())
+    form.submit(store());
 }
 </script>
 
@@ -22,7 +23,7 @@ function submit() {
     <Head title="Register" />
 
     <AuthLayout>
-        <h2 class="text-2xl font-semibold text-center mb-4">Create an account</h2>
+        <h2 class="mb-4 text-center text-2xl font-semibold">Create an account</h2>
         <form @submit.prevent="submit" class="flex flex-col gap-6">
             <div class="grid gap-6">
                 <UFormField name="name" :error="form.errors.name" label="Name">
@@ -34,55 +35,51 @@ function submit() {
                 </UFormField>
 
                 <UFormField name="password" :error="form.errors.password" label="Password">
-                  <UInput
-                    v-model="form.password"
-                    class="w-full"
-                    :type="showPassword ? 'text' : 'password'"
-                    autocomplete="new-password"
-                    placeholder="Password"
-                    :ui="{ trailing: 'pe-1' }"
-                    required
-                  >
-                    <template #trailing>
-                      <UButton
-                        color="neutral"
-                        variant="link"
-                        size="sm"
-                        :icon="showPassword ? 'i-lucide-eye-off' : 'i-lucide-eye'"
-                        :aria-label="showPassword ? 'Hide password' : 'Show password'"
-                        :aria-pressed="showPassword"
-                        @click="showPassword = !showPassword"
-                      />
-                    </template>
-                  </UInput>
+                    <UInput
+                        v-model="form.password"
+                        class="w-full"
+                        :type="showPassword ? 'text' : 'password'"
+                        autocomplete="new-password"
+                        placeholder="Password"
+                        :ui="{ trailing: 'pe-1' }"
+                        required
+                    >
+                        <template #trailing>
+                            <UButton
+                                color="neutral"
+                                variant="link"
+                                size="sm"
+                                :icon="showPassword ? 'i-lucide-eye-off' : 'i-lucide-eye'"
+                                :aria-label="showPassword ? 'Hide password' : 'Show password'"
+                                :aria-pressed="showPassword"
+                                @click="showPassword = !showPassword"
+                            />
+                        </template>
+                    </UInput>
                 </UFormField>
 
-                <UFormField
-                  name="password_confirmation"
-                  :error="form.errors.password_confirmation"
-                  label="Confirm Password"
-                >
-                  <UInput
-                    v-model="form.password_confirmation"
-                    class="w-full"
-                    :type="showPasswordConfirmation ? 'text' : 'password'"
-                    autocomplete="new-password"
-                    placeholder="Confirm Password"
-                    :ui="{ trailing: 'pe-1' }"
-                    required
-                  >
-                    <template #trailing>
-                      <UButton
-                        color="neutral"
-                        variant="link"
-                        size="sm"
-                        :icon="showPasswordConfirmation ? 'i-lucide-eye-off' : 'i-lucide-eye'"
-                        :aria-label="showPasswordConfirmation ? 'Hide password' : 'Show password'"
-                        :aria-pressed="showPasswordConfirmation"
-                        @click="showPasswordConfirmation = !showPasswordConfirmation"
-                      />
-                    </template>
-                  </UInput>
+                <UFormField name="password_confirmation" :error="form.errors.password_confirmation" label="Confirm Password">
+                    <UInput
+                        v-model="form.password_confirmation"
+                        class="w-full"
+                        :type="showPasswordConfirmation ? 'text' : 'password'"
+                        autocomplete="new-password"
+                        placeholder="Confirm Password"
+                        :ui="{ trailing: 'pe-1' }"
+                        required
+                    >
+                        <template #trailing>
+                            <UButton
+                                color="neutral"
+                                variant="link"
+                                size="sm"
+                                :icon="showPasswordConfirmation ? 'i-lucide-eye-off' : 'i-lucide-eye'"
+                                :aria-label="showPasswordConfirmation ? 'Hide password' : 'Show password'"
+                                :aria-pressed="showPasswordConfirmation"
+                                @click="showPasswordConfirmation = !showPasswordConfirmation"
+                            />
+                        </template>
+                    </UInput>
                 </UFormField>
 
                 <UButton :loading="form.processing" type="submit" block class="mt-4">Log in</UButton>
@@ -91,7 +88,7 @@ function submit() {
 
         <p class="mt-4 text-center text-sm text-gray-600">
             Already have an account?
-            <Link href="/login" class="text-blue-500 hover:underline">Login</Link>
-        </p>
-    </AuthLayout>
+            <TextLink :href="login()" class="text-sm font-medium text-primary">Log In</TextLink>
+        </p></AuthLayout
+    >
 </template>
