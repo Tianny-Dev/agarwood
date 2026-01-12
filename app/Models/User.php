3319a6326f566929d3f176e431f65caba52a9123
Ahllaine Christian De Ocera <acdeocera.dev@gmail.com>
2026-01-12 13:57:04 +0800

    fix: removed the verified email trait on User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ use Illuminate\Notifications\Notifiable;
 use Spatie\Sluggable\HasSlug;
 use Spatie\Sluggable\SlugOptions;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, HasSlug, Notifiable;
