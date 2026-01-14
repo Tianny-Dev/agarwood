1953e3e447d0e4126f60ff884d9fa4127e06d118
Ahllaine Christian De Ocera <acdeocera.dev@gmail.com>
2026-01-14 10:51:53 +0800

    feat(migration): added migration for agent accounts

---
+++ database/migrations/2026_01_14_104023_create_agents_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('agents', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->unique()->constrained()->cascadeOnDelete();
+
+            $table->string('agent_code')->unique(); 
+            $table->boolean('is_verified')->default(false);
+            $table->timestamp('verified_at')->nullable();
+            $table->foreignId('verified_by')->nullable()->constrained('users');
+
+            $table->timestamps();
+        });
+
+        Schema::table('investors', function (Blueprint $table) {
+            $table->foreignId('agent_id')->after('user_id')->nullable()->constrained('agents')->nullOnDelete();
+        });
+
+        Schema::table('partners', function (Blueprint $table) {
+            $table->foreignId('agent_id')->after('user_id')->nullable()->constrained('agents')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('investors', function (Blueprint $table) {
+            $table->dropForeign(['agent_id']);
+            $table->dropColumn('agent_id');
+        });
+
+        Schema::table('partners', function (Blueprint $table) {
+            $table->dropForeign(['agent_id']);
+            $table->dropColumn('agent_id');
+        });
+
+        Schema::dropIfExists('agents');
+    }
+};
